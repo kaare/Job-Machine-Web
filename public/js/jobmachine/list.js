af0abd8caf2b726062c3f40e3d6949ee4c87a831
Kaare Rasmussen <kaare@jasonic.dk>
2011-05-07 19:00:36 +0200

    Javascript to format json data

--- public/js/jobmachine/list.js
+++ public/js/jobmachine/list.js
@@ -3,4 +3,10 @@ jQuery(document).ready(function(){
 		var $form = $(this).closest("form");
 		$(this).closest("form").submit();
 	});
+	$(".json").each(function () {
+		var parm = $(this);
+		var json = eval('(' + parm.html() + ')');
+		var p = JSON.stringify(json, null, 4);
+		$(this).html(p);
+	});
 });
